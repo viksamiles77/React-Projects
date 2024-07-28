a109e85dec10e299369ea478d6a35299951f6609
viksamiles77 <mileskiviktor77@gmail.com>
2024-07-28 20:29:06 +0200

    Date Counter v2 Initial

---
+++ Date Counter v2/date-counter-v2/src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("July 04 2004");
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is`
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button className="reset-btn" onClick={handleReset}>
+            RESET
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
