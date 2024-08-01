c3c9e3c2497c10f0a687212450defdfefea46868
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-01 17:04:31 +0200

    Update AccordionItem.js

--- FAQ App/faq-app/src/components/AccordionItem.js
+++ FAQ App/faq-app/src/components/AccordionItem.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
-
-export default function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function AccordionItem({
+  num,
+  title,
+  curOpen,
+  onOpen,
+  children,
+}) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -12,7 +16,7 @@ export default function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
