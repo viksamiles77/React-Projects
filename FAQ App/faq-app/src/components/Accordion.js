8d41bfe6ad557791b0685cb7f32e551b22f0caa2
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-01 17:04:42 +0200

    Update Accordion.js

--- FAQ App/faq-app/src/components/Accordion.js
+++ FAQ App/faq-app/src/components/Accordion.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
