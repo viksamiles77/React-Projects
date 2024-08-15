79f4b438cc80d2e2b917f467f438972d82918fcc
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-15 17:09:02 +0200

    eat-n-split init

---
+++ Eat-N Split/eat-n-split/README.md
@@ -0,0 +1,15 @@
+## Eat-N Split App
+
+Eat-N Split is a React application designed to simplify the process of splitting bills among friends. The app allows users to add friends, input their expenses, and easily calculate how much each person owes or is owed.
+
+## Features
+
+- Add Friends: Users can add friends to the list by entering a name and an optional image URL.
+- View Friends: The app displays a list of friends, showing their current balance (whether they owe money or are owed).
+- Split Bills: Users can select a friend, input the bill amount, and the app will calculate how much each person needs to pay. The balance is updated accordingly.
+
+## Technologies Used
+
+- React
+- JavaScript
+- CSS
---
+++ Eat-N Split/eat-n-split/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { initialFriends } from "./data/initialFriends";
+import { Button } from "./components/Button";
+import { FriendsList } from "./components/FriendsList";
+import { FormAddFriend } from "./components/FormAddFriend";
+import { FormSplitBill } from "./components/FormSplitBill";
+
+export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  function handleSelection(friend) {
+    // setSelectedFriend(friend);
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
+  }
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList
+          friends={friends}
+          selectedFriend={selectedFriend}
+          onSelection={handleSelection}
+        />
+
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
+      )}
+    </div>
+  );
+}
