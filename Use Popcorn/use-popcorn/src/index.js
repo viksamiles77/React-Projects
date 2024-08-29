8dcdf9dde10091ee4e00ca5dd1692b3a4433340a
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-29 03:10:27 +0200

    Update index.js

--- Use Popcorn/use-popcorn/src/index.js
+++ Use Popcorn/use-popcorn/src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import StarRating from "./StarRating";
-
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-//   <div>
-//     <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
-//     <p>This movies was rated {movieRating} stars</p>
-//   </div>;
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
